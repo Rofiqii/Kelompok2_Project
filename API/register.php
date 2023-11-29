237930b4201b2f592a7774034f83c47610919051
SyaifWafy <wafy.gray@gmail.com>
2023-11-29 12:25:52 +0700

    API-register fixed

--- API/register.php
+++ API/register.php
@@ -4,15 +4,21 @@ require_once '../koneksi.php';
 $values = file_get_contents('php://input');
 $data = json_decode($values);
 
-$query = mysqli_query($koneksi, "INSERT INTO customer (username_cus, fullname_cus, password_cus, pertanyaan, jawaban) VALUES ('{$data->username_cus}', '{$data->fullname_cus}', '{$data->password_cus}', '{$data->pertanyaan}', '{d$ata->jawaban}')");
+$query = mysqli_query($koneksi, "INSERT INTO customer (username_cus, fullname_cus, password_cus, pertanyaan, jawaban) VALUES ('{$data->username_cus}', '{$data->fullname_cus}', '{$data->password_cus}', '{$data->pertanyaan}', '{$data->jawaban}')");
 
 $affectedRows = mysqli_affected_rows($koneksi);
 
 header('Content-Type: application/json');
 
-if ($affectedRows > 0)
+if ($affectedRows > 0) {
     echo json_encode([
         'status' => 'ok',
         'pesan' => 'Register berhasil',
     ]);
+} else {
+    echo json_encode([
+        'status' => 'error',
+        'pesan' => 'Gagal melakukan registrasi',
+    ]);
+}
 ?>
